8c8cb74beca3dfc1238e50ef431ff473be1e95f6
Edward Barker <epabarker@gmail.com>
2016-02-17 23:56:19 +0000

    Added How are you
    Just did what the exercise said!

--- code/ch01/Hello.java
+++ code/ch01/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         // generate some simple output
         System.out.println("Hello, World!");
+        System.out.println("How are you?");
+        // this comment is included just to test I know what I am doing
     }
 }
